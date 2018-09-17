6f456a49b4c5f1a210bef8e54fd3082f102c856a
bgarciaoliveira <brunogarcia212@gmail.com>
2018-09-17 18:03:01 -0300

    criação rotas para tarefas

--- src/main/java/br/garcia/resource/TarefaResource.java
+++ src/main/java/br/garcia/resource/TarefaResource.java
@@ -4,10 +4,12 @@ import br.garcia.service.ColaboradorService;
 import br.garcia.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.annotation.Resource;
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping(
@@ -19,4 +21,34 @@ public class TarefaResource {
 
     @Autowired
     private TarefaService tarefaService;
+
+    @RequestMapping(value = "/{colaboradorId}")
+    public ResponseEntity getAll(@PathVariable String colaboradorId){
+        throw new NotImplementedException();
+    }
+
+    @RequestMapping(value = "/{colaboradorId}/{id}")
+    public ResponseEntity getById(@PathVariable String colaboradorId, String id){
+        throw new NotImplementedException();
+    }
+
+    @PutMapping(value = "/updateNome")
+    public ResponseEntity updateNome(@RequestBody String json){
+        throw new NotImplementedException();
+    }
+
+    @PutMapping(value = "/updateDescricao")
+    public ResponseEntity updateDescricao(@RequestBody String json){
+        throw new NotImplementedException();
+    }
+
+    @PutMapping(value = "/updateStatus")
+    public ResponseEntity updateStatus(@RequestBody String json){
+        throw new NotImplementedException();
+    }
+
+    @DeleteMapping(value = "/{colaboradorId}/{id}")
+    public ResponseEntity delete(@PathVariable String colaboradorId, String id){
+        throw new NotImplementedException();
+    }
 }
